1d215e60782b75b29dce43a5e43f4cca22899c78
physicspenguin <segel-flieger@gmx.at>
2021-12-18 18:15:42 +0100

    addition fixed for commutativity

--- cga_py/multivector.py
+++ cga_py/multivector.py
@@ -3,7 +3,9 @@ import numpy as np
 class cga_object:
 
     """
-
+    Element of the CGA with methods for:
+    addition, multiplication,
+    printing
 
     """
     dim = 32
@@ -42,10 +44,14 @@ class cga_object:
                    "e_123io"]
 
     def __init__(self, gen):
+        if isinstance(gen,cga_object):
+            cof = gen.coeff
+        else:
+            cof = gen
         ## Version if list is given
         self.coeff = np.zeros(self.dim)
-        for i in range(len(gen)):
-            self.coeff[i] = gen[i]
+        for i in range(len(cof)):
+            self.coeff[i] = cof[i]
 
     def __add__(self, other):
         """Addition of cga_object
@@ -56,7 +62,12 @@ class cga_object:
         Returns: addition of cga_object with cga_object
 
         """
-        coefficients = self.coeff + other.coeff
+        # This is a rather hacky way of ensuring that addition of different
+        # datatypes is commutative. Pending better implementation
+        try:
+            coefficients = self.coeff + other.coeff
+        except:
+            coefficients = (other + self).coeff
         return cga_object(coefficients)
 
     def __radd__(self, other):
@@ -100,9 +111,41 @@ class cga_object:
                     out += " + "+str(self.coeff[i])+self.coeff_names[i]
         print(out)
 
+########################################
+# Base Objects
+########################################
 
-
-
+e_1     = cga_object(np.eye(32)[:,1])
+e_2     = cga_object(np.eye(32)[:,2])
+e_3     = cga_object(np.eye(32)[:,3])
+e_i     = cga_object(np.eye(32)[:,4])
+e_o     = cga_object(np.eye(32)[:,5])
+e_12    = cga_object(np.eye(32)[:,6])
+e_13    = cga_object(np.eye(32)[:,7])
+e_1i    = cga_object(np.eye(32)[:,8])
+e_1o    = cga_object(np.eye(32)[:,9])
+e_23    = cga_object(np.eye(32)[:,10])
+e_2i    = cga_object(np.eye(32)[:,11])
+e_2o    = cga_object(np.eye(32)[:,12])
+e_3i    = cga_object(np.eye(32)[:,13])
+e_3o    = cga_object(np.eye(32)[:,14])
+e_io    = cga_object(np.eye(32)[:,15])
+e_123   = cga_object(np.eye(32)[:,16])
+e_12i   = cga_object(np.eye(32)[:,17])
+e_12o   = cga_object(np.eye(32)[:,18])
+e_13i   = cga_object(np.eye(32)[:,19])
+e_13o   = cga_object(np.eye(32)[:,20])
+e_1io   = cga_object(np.eye(32)[:,21])
+e_23i   = cga_object(np.eye(32)[:,22])
+e_23o   = cga_object(np.eye(32)[:,23])
+e_2io   = cga_object(np.eye(32)[:,24])
+e_3io   = cga_object(np.eye(32)[:,25])
+e_123i  = cga_object(np.eye(32)[:,26])
+e_123o  = cga_object(np.eye(32)[:,27])
+e_12io  = cga_object(np.eye(32)[:,28])
+e_13io  = cga_object(np.eye(32)[:,29])
+e_23io  = cga_object(np.eye(32)[:,30])
+e_123io = cga_object(np.eye(32)[:,31])
 
 
 ########################################
