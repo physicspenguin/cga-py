rsub error fixed and four quaternions more tested

cga_py/operators.py
@@ -11,10 +11,12 @@ def act(rotator, obj):
         rotator to apply to obj
     obj : cga_object
         Object to be acted upon
-        Returns: (cga_object) Rotated object
+
 
     Returns
     -------
+    cga_object
+        Rotated object
 
     """
     return rotator * obj * (~rotator)
@@ -26,13 +28,11 @@ def com(a, b):
     Parameters
     ----------
     a : cga_object
-        TODO
     b : cga_object
-        TODO
-        Returns: cga_object
 
     Returns
     -------
+    cga_object
 
     """
     return (1 / 2) * (a * b - b * a)
@@ -44,13 +44,11 @@ def anti_com(a, b):
     Parameters
     ----------
     a : cga_object
-        TODO
     b : cga_object
-        TODO
-        Returns: cga_object
 
     Returns
     -------
+    cga_object
 
     """
     return (1 / 2) * (a * b + b * a)
@@ -65,13 +63,14 @@ def n_grade(obj, grade):
         object of which to extract grade-graded parts
     grade : int
         grade of parts to be extracted
-        Returns: (cga_object) grade-graded parts of obj
 
     Returns
     -------
+    cga_object
+        grade-graded parts of obj
 
     """
-    vec = np.zeros(32)
+    vec = np.zeros_like(obj.coeff)
     if grade == 0:
         ind = 0
         vec[ind] = obj.coeff[ind]
@@ -99,7 +98,7 @@ def n_grade(obj, grade):
 
 
 def r_norm(obj):
-    """Return obj*(obj^*) in CGA
+    """Calculates the right norm in CGA
 
     Parameters
     ----------
@@ -108,7 +107,7 @@ def r_norm(obj):
 
     Returns
     -------
-    type
+    cga_object
 
 
     """
@@ -116,7 +115,7 @@ def r_norm(obj):
 
 
 def l_norm(obj):
-    """Return (obj^*)*obj in CGA
+    """Calculates the left norm in CGA
 
     Parameters
     ----------
@@ -125,7 +124,7 @@ def l_norm(obj):
 
     Returns
     -------
-    type
+    cga_object
 
 
     """

cga_py/random_gen.py
@@ -128,7 +128,7 @@ def rand_rotor(maximum=10, tol=0):
         )
 
         if a[15] == 0:
-            pass
+            continue
         a[0] = (a[5] * a[10] - a[6] * a[9] + a[7] * a[8]) / a[15]
         a[1] = (a[5] * a[13] - a[6] * a[12] + a[7] * a[11]) / a[15]
         a[2] = (a[5] * a[14] - a[8] * a[12] + a[9] * a[11]) / a[15]
@@ -137,7 +137,7 @@ def rand_rotor(maximum=10, tol=0):
         out = cga_object(a, True)
         # This could be replaced by numpy.testing.is_close in the future
         if not (np.linalg.norm(study_var(out)) <= tol):
-            pass
+            continue
 
         return out
 
@@ -183,13 +183,13 @@ def rand_rot_poly(maximum=10, tol=0):
         )
 
         if a[10] == 0:
-            pass
+            continue
         a[1] = (a[3] * a[7] - a[4] * a[6]) / a[10]
         a[2] = (a[3] * a[9] - a[4] * a[8]) / a[10]
         a[5] = (a[6] * a[9] - a[7] * a[8]) / a[10]
         out = cga_object(a, True)
         if np.linalg.norm(study_var(out)) > tol:
-            pass
+            continue
         return out
 
 